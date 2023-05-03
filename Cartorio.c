dc98f1f2b27674ee44b5e6da05ebfe4c34449843
herissonnogueira <128763446+herissonnogueira@users.noreply.github.com>
2023-05-02 22:12:31 -0300

    Opção de saída
    Incluso no sistema a opção de saída para o usuário.

--- Cartorio.c
+++ Cartorio.c
@@ -134,6 +134,7 @@ int main()
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar os nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); 
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Esse Software foi feito por Herisson Nogueira \n"); //Créditos
 		printf("Opção: ");//fim do menu
 	
@@ -155,6 +156,10 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por ultilizar o sitema!\n");
+			return 0;
 			
 			default:
 			printf("Essa opção não está disponivel!\n");
